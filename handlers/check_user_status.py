3a2a8906947b0cd9d9f8682c9f3c730657306c07
Snackloosee <edosqtf358@gmail.com>
2024-06-24 08:12:12 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -23,7 +23,7 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You are Banned to Use This Bot baby 🥺", quote=True)
+            await cmd.reply_text("You can not use the bot, Because you are banned. Contact here @kdrama_girlchat", quote=True)
             return
     await cmd.continue_propagation()
     
